2ddbe0524900bd4549b7d2c9d641a1d341124433
mythilibiju <mythilibiju2006@gmail.com>
2025-01-05 23:46:24 +0530

    commited files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import Add from './pages/Add';
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+
 function Cards() {
     const [data, setData] = useState([]);
 
@@ -8,31 +9,30 @@ function Cards() {
             .then((response) => setData(response.data))
             .catch((error) => console.error('Error fetching data:', error));
     }, []);
-  return (
-    <div style={{
-      
-        display: 'grid', 
-        gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))', 
-        gap: '10px', 
-        padding: '10px'
-    }}>
-        {data.map((task) => (
-            <div style={{
-                backgroundColor:'lightgray',
-                border: '1px solid darkgrey', 
-                padding: '10px', 
-                textAlign: 'center',
-                width:'185px'
-            }}>
-                {<img src={task.image} style={{width: '100px', height: '100px', objectFit: 'cover'}} />}<br />
-                {task.title}<br />
-                {task.category}<br />
-                {task.price}
-            </div>
-        ))}
-    </div>
-    
-  )
+
+    return (
+        <div style={{
+            display: 'grid',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
+            gap: '10px',
+            padding: '10px'
+        }}>
+            {data.map((task, index) => (
+                <div key={index} style={{
+                    backgroundColor: 'lightgray',
+                    border: '1px solid darkgrey',
+                    padding: '10px',
+                    textAlign: 'center',
+                    width: '185px'
+                }}>
+                    <img src={task.image} alt={task.title} style={{ width: '100px', height: '100px', objectFit: 'cover' }} />
+                    <div>{task.title}</div>
+                    <div>{task.category}</div>
+                    <div>${task.price}</div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
-export default Cards
+export default Cards;
